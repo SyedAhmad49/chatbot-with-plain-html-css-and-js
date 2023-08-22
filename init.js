035aba8da3bbb8e12377930a63b6e3a842077e35
Ahmad4993 <ahmad.farooq@ray-solutions.co.uk>
2023-08-22 16:09:50 +0500

    observer added so that iframe height should be dynamic depending on its block and none styling.

--- init.js
+++ init.js
@@ -1,4 +1,3 @@
-
 const chatbotId = window.chatbaseConfig.chatbotId;
 console.log(chatbotId)
 // Set iframe attributes
@@ -8,11 +7,57 @@ iframe.id = "chat-init";
 // iframe.src = "https://chat.ray-solutions.co.uk/index.html";
 iframe.src = "./Hobot.html";
 iframe.width = "100%";
-iframe.style.minWidth = "420px";
-iframe.style.height = "100%";
+// iframe.style.minWidth = "420px";
+// iframe.style.maxWidth = "420px";
+iframe.style.width = "100px"
+iframe.style.height = "100px";
 iframe.style.right = "0";
 iframe.style.position = "fixed";
 iframe.style.bottom = "10px"
 iframe.frameBorder = "0";
+iframe.onload = iframeLoaded
 document.body.appendChild(iframe)
+let observer;
+
+function iframeLoaded() {
+    console.log("Iframe has finished loading.");
+
+    // Get the iframe element
+    const iframe = document.getElementById("chat-init");
+
+    // Get the iframe's content window and document
+    const iframeWindow = iframe.contentWindow || iframe.contentDocument.defaultView;
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+    // Access elements within the iframe's document
+    var chatContainer = iframeDocument.getElementById("myForm");
+
+    if (chatContainer) {
+        // Create a MutationObserver to observe changes in the style attribute
+        observer = new MutationObserver(mutations => {
+            const displayProperty = window.getComputedStyle(chatContainer);
+            if(displayProperty.display === "none") {
+                iframe.style.height = '100px';
+                iframe.style.width = '100px';
+            } else {
+                iframe.style.height = '80%';
+                iframe.style.width = '442px';
+            }
+        });
+
+        // Observe changes to the style attribute of the chatContainer
+        observer.observe(chatContainer, { attributes: true, attributeFilter: ["style"] });
+    } else {
+        console.log("Chat container not found.");
+    }
+}
+// Disconnect the observer when you're done
+// if (observer) {
+//     observer.disconnect();
+// }
+
+
+
+
+
 
